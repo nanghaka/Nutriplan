0d5b06fab9886ed06bdaa6b0465fe7cb53613d4f
codephillip <codephillip@gmail.com>
2015-06-19 19:09:55 +0300

    tested number picker

--- app/src/main/java/com/app/cryptotunnel/nutriplan/Calories.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/Calories.java
@@ -9,42 +9,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class Calories extends Fragment {
 
     TextView textView;
     Button button;
-//    NumberPicker np;
-//    TextView tv1, tv2;
+    NumberPicker np;
+    TextView tv1, tv2;
 
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.first_layout, container, false);
-//
-//        np = (NumberPicker) rootView.findViewById(R.id.numberPicker1);
-//        tv1 = (TextView) rootView.findViewById(R.id.textView2);
-//        tv2 = (TextView) rootView.findViewById(R.id.textView3);
-//
-//        np.setMinValue(0);
-//        np.setMaxValue(10);
-//        np.setWrapSelectorWheel(false);
-//
-//        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//                // TODO Auto-generated method stub
-//
-//                String Old = "Old Value : ";
-//                String New = "New Value : ";
-//
-//                tv1.setText(Old.concat(String.valueOf(oldVal)));
-//                tv2.setText(New.concat(String.valueOf(newVal)));
-//            }
-//        });
+
+        np = (NumberPicker) rootView.findViewById(R.id.numberPicker1);
+        tv1 = (TextView) rootView.findViewById(R.id.textView2);
+        tv2 = (TextView) rootView.findViewById(R.id.textView3);
+
+        np.setMinValue(0);
+        np.setMaxValue(10);
+        np.setWrapSelectorWheel(false);
+
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                // TODO Auto-generated method stub
+
+                String Old = "Old Value : ";
+                String New = "New Value : ";
+
+                tv1.setText(Old.concat(String.valueOf(oldVal)));
+                tv2.setText(New.concat(String.valueOf(newVal)));
+            }
+        });
 
 
 //        textView = (TextView) rootView.findViewById(R.id.words);
