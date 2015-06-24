c4e5ad8d53677256bbbc4222bd98e9cf85ef9f3b
codephillip <codephillip@gmail.com>
2015-06-24 18:47:47 +0300

    added add-icons to diary action bar ..incomplete

--- app/src/main/java/com/app/cryptotunnel/nutriplan/MealDairy.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/MealDairy.java
@@ -3,6 +3,8 @@ package com.app.cryptotunnel.nutriplan;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -135,6 +137,26 @@ public class MealDairy extends ActionBarActivity {
             }
 
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_meal_dairy, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
