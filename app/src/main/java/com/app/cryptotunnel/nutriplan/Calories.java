5c60092d303efd0e3e0c5a55b3824b2981cd9228
codephillip <codephillip@gmail.com>
2015-06-25 16:34:49 +0300

    added gender radio buttons to calories fragment

--- app/src/main/java/com/app/cryptotunnel/nutriplan/Calories.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/Calories.java
@@ -8,81 +8,76 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
-public class Calories extends Fragment {
+public class Calories extends Fragment implements RadioGroup.OnCheckedChangeListener {
 
-//    NumberPicker np;
+    //    NumberPicker np;
 //    TextView tv1, tv2;
-    EditText weight,height,age;
+    EditText weight, height, age;
     Button calculate;
-    double w,h,a,caloriesm,caloriesf;
+    RadioGroup gender;
+    double w, h, a, calories;
     TextView show;
 
 
-	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.first_layout, container, false);
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.first_layout, container, false);
 
 
-        weight= (EditText) rootView.findViewById(R.id.editText);
-        height= (EditText) rootView.findViewById(R.id.editText2);
-        age= (EditText) rootView.findViewById(R.id.editText3);
-        calculate=(Button) rootView.findViewById(R.id.calculate);
-        show=(TextView) rootView.findViewById(R.id.show);
+        weight = (EditText) rootView.findViewById(R.id.editText);
+        height = (EditText) rootView.findViewById(R.id.editText2);
+        age = (EditText) rootView.findViewById(R.id.editText3);
+        calculate = (Button) rootView.findViewById(R.id.calculate);
+        show = (TextView) rootView.findViewById(R.id.show);
+        gender = (RadioGroup) rootView.findViewById(R.id.gender);
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                w=Double.parseDouble(String.valueOf(weight.getText()));
-                h=Double.parseDouble(String.valueOf(height.getText()));
-                a=Double.parseDouble(String.valueOf(age.getText()));
-                caloriesm=((10*w)+(6.25*h)-(5*a)+5);
-                caloriesf=((10*w)+(6.25*h)-(5*a)-161);
+//                w = Double.parseDouble(String.valueOf(weight.getText()));
+//                h = Double.parseDouble(String.valueOf(height.getText()));
+//                a = Double.parseDouble(String.valueOf(age.getText()));
+//                caloriesm = ((10 * w) + (6.25 * h) - (5 * a) + 5);
+//                caloriesf = ((10 * w) + (6.25 * h) - (5 * a) - 161);
 
-               show.setText(String.valueOf(caloriesm));
+                show.setText(String.valueOf(calories));
             }
         });
 
+        gender.setOnCheckedChangeListener(this);
 
 
-//        np = (NumberPicker) rootView.findViewById(R.id.numberPicker1);
-//        tv1 = (TextView) rootView.findViewById(R.id.textView2);
-//        tv2 = (TextView) rootView.findViewById(R.id.textView3);
-//
-//        np.setMinValue(0);
-//        np.setMaxValue(10);
-//        np.setWrapSelectorWheel(false);
-//
-//        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//                // TODO Auto-generated method stub
-//
-//                String Old = "Old Value : ";
-//                String New = "New Value : ";
-//
-//                tv1.setText(Old.concat(String.valueOf(oldVal)));
-//                tv2.setText(New.concat(String.valueOf(newVal)));
-//            }
-//        });
+        return rootView;
+    }
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.male:
+                w = Double.parseDouble(String.valueOf(weight.getText()));
+                h = Double.parseDouble(String.valueOf(height.getText()));
+                a = Double.parseDouble(String.valueOf(age.getText()));
+                calories = ((10 * w) + (6.25 * h) - (5 * a) + 5);
+                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.4);
+                //show.setText(proteinResult.toString());
+                break;
+            case R.id.female:
+                w = Double.parseDouble(String.valueOf(weight.getText()));
+                h = Double.parseDouble(String.valueOf(height.getText()));
+                a = Double.parseDouble(String.valueOf(age.getText()));
+                calories = ((10 * w) + (6.25 * h) - (5 * a) - 161);
+                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.4);
+                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.6);
+                //show.setText(proteinResult.toString());
+                break;
 
-//        textView = (TextView) rootView.findViewById(R.id.words);
-//        button= (Button) rootView.findViewById(R.id.click);
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                textView.setText("you have clicked");
-//            }
-//        });
-
-
-		return rootView;
-	}
+        }
+    }
+}
 //    public void show()
 //    {
 //
@@ -121,4 +116,37 @@ public class Calories extends Fragment {
 //    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 //
 //    }
-}
+    //        np = (NumberPicker) rootView.findViewById(R.id.numberPicker1);
+//        tv1 = (TextView) rootView.findViewById(R.id.textView2);
+//        tv2 = (TextView) rootView.findViewById(R.id.textView3);
+//
+//        np.setMinValue(0);
+//        np.setMaxValue(10);
+//        np.setWrapSelectorWheel(false);
+//
+//        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//
+//            @Override
+//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+//                // TODO Auto-generated method stub
+//
+//                String Old = "Old Value : ";
+//                String New = "New Value : ";
+//
+//                tv1.setText(Old.concat(String.valueOf(oldVal)));
+//                tv2.setText(New.concat(String.valueOf(newVal)));
+//            }
+//        });
+
+
+//        textView = (TextView) rootView.findViewById(R.id.words);
+//        button= (Button) rootView.findViewById(R.id.click);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textView.setText("you have clicked");
+//            }
+//        });
+
+
