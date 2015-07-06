bd104eaea3f0e701b61b38266537c563417a92c3
codephillip <codephillip@gmail.com>
2015-07-06 16:51:48 +0300

    added the app logo

--- app/src/main/java/com/app/cryptotunnel/nutriplan/WeightLoss.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/WeightLoss.java
@@ -7,7 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.view.LineChartView;
 
 public class WeightLoss extends Fragment {
@@ -23,6 +29,23 @@ public class WeightLoss extends Fragment {
 		lineChartView.setZoomType(ZoomType.HORIZONTAL_AND_VERTICAL);
 		//lineChartView.setContainerScrollEnabled(true,container);
 
+		List<PointValue> values = new ArrayList<PointValue>();
+		values.add(new PointValue(8, 5));
+		values.add(new PointValue(8, 8));
+		values.add(new PointValue(2, 1));
+		values.add(new PointValue(3, 4));
+
+		//In most cased you can call data model methods in builder-pattern-like manner.
+		Line line = new Line(values).setColor(R.color.blue).setCubic(true);
+		List<Line> lines = new ArrayList<Line>();
+		lines.add(line);
+
+		LineChartData data = new LineChartData();
+		data.setLines(lines);
+
+//		LineChartView chart = new LineChartView(context);
+//		chart.setLineChartData(data);
+		lineChartView.setLineChartData(data);
 
 
 		return rootView;
