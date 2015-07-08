637f9d655de8b7cd259704a85e8f19bb089054ec
codephillip <codephillip@gmail.com>
2015-07-08 13:32:27 +0300

    added editText field and save button to weightloss

--- app/src/main/java/com/app/cryptotunnel/nutriplan/WeightLoss.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/WeightLoss.java
@@ -4,17 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class WeightLoss extends Fragment {
+public class WeightLoss extends Fragment implements  View.OnClickListener {
 	//LineChartView lineChartView;
-	Button graph;
-	Button history;
-
-	DatabaseHandler db = new DatabaseHandler(getActivity());
+	Button graph,history,save;
+	EditText currentWeight;
+	//Button history;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
@@ -44,33 +45,62 @@ public class WeightLoss extends Fragment {
 ////		chart.setLineChartData(data);
 //		lineChartView.setLineChartData(data);
 
+		save = (Button) rootView.findViewById(R.id.save);
+		currentWeight = (EditText) rootView.findViewById(R.id.currentWeightEdit);
 		history = (Button) rootView.findViewById(R.id.history);
 		graph= (Button) rootView.findViewById(R.id.graph);
-		graph.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getActivity(), LineChartActivity.class));
-			}
-		});
-
-		history.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
-				startActivity(new Intent(getActivity(), History.class));
-//				Log.d("SQL Insert: ", "Inserting ..");
-//				db.addWeight(new WeightTrackerContract("36"));
-//				Log.d("SQL Insert: ", "Inserting 36");
-//				db.addWeight(new WeightTrackerContract("56"));
-//				Log.d("SQL Insert: ", "Inserting 56");
+		graph.setOnClickListener(this);
+		history.setOnClickListener(this);
+		save.setOnClickListener(this);
+//		graph.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				startActivity(new Intent(getActivity(), LineChartActivity.class));
+//			}
+//		});
 //
-//				Toast.makeText(getActivity(), "Note has been saved", Toast.LENGTH_SHORT).show();
-			}
-		});
+//		history.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//
+//				startActivity(new Intent(getActivity(), History.class));
+////				Log.d("SQL Insert: ", "Inserting ..");
+////				db.addWeight(new WeightTrackerContract("36"));
+////				Log.d("SQL Insert: ", "Inserting 36");
+////				db.addWeight(new WeightTrackerContract("56"));
+////				Log.d("SQL Insert: ", "Inserting 56");
+////
+////				Toast.makeText(getActivity(), "Note has been saved", Toast.LENGTH_SHORT).show();
+//			}
+//		});
 
 
 
 
 		return rootView;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+			case R.id.history:
+				startActivity(new Intent(getActivity(), History.class));
+				break;
+
+			case R.id.graph:
+				startActivity(new Intent(getActivity(), LineChartActivity.class));
+				break;
+
+			case R.id.save:
+				DatabaseHandler db = new DatabaseHandler(getActivity());
+				Log.d("SQL Insert: ", "Inserting ..");
+				db.addWeight(new WeightTrackerContract(currentWeight.getText().toString(),NoteEditorActivity.getTime()));
+
+
+				break;
+
+		}
+
+
 	}
 }
