7bf61f00862cca919d1bae9bece8fa6c8ad89074
codephillip <codephillip@gmail.com>
2015-07-11 20:55:26 +0300

    added bar scanning library and simpleScannerActivity code

--- app/src/main/java/com/app/cryptotunnel/nutriplan/WeeklyPlan.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/WeeklyPlan.java
@@ -1,34 +1,39 @@
 package com.app.cryptotunnel.nutriplan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class WeeklyPlan extends Fragment {
 //
 //    TextView textView;
-//    Button button;
+    Button button;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.weelyplan, container, false);
 
+
+
 		//startActivity(new Intent(getActivity(),LineChartActivity.class));
 
 
        // textView = (TextView) rootView.findViewById(R.id.words);
-//        button= (Button) rootView.findViewById(R.id.click);
+        button= (Button) rootView.findViewById(R.id.click);
 //
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                textView.setText("you have clicked");
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //textView.setText("you have clicked");
+				startActivity(new Intent(getActivity(), SimpleScannerActivity.class));
+            }
+        });
 
 
 		return rootView;
