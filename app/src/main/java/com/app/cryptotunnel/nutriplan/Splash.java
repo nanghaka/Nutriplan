3c662e1cf8507cfccd71dced2e0dab9befdf58fb
codephillip <codephillip@gmail.com>
2015-07-19 00:14:21 +0300

    added splash screen activity

---
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/Splash.java
@@ -0,0 +1,31 @@
+package com.app.cryptotunnel.nutriplan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timer =new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent openMainActivity =new Intent(Splash.this,LoginActivity.class);
+                    startActivity(openMainActivity);
+                    finish();
+
+                }
+            }
+        };
+        timer.start();
+    }
+
+}
