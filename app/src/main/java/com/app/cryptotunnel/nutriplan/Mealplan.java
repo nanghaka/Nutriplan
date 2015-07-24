444b4a3b3bb67f21e72bd5d8f824436160934b1d
codephillip <codephillip@gmail.com>
2015-07-24 18:55:13 +0300

    added onclicklistener to imageButtons and added breakfast, lunch and dinner

--- app/src/main/java/com/app/cryptotunnel/nutriplan/Mealplan.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/Mealplan.java
@@ -3,43 +3,84 @@ package com.app.cryptotunnel.nutriplan;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class Mealplan extends Fragment {
-//
-//    TextView textView;
-    Button button;
+public class Mealplan extends Fragment implements  View.OnClickListener{
 
+
+    ImageButton next, previous;
+    TextView dayoftheweek, breakfastFood, lunchFood, dinnerFood;
+    String days[] ={
+      "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    };
+    int counter = 0;
+    final int n=days.length;
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.mealplan, container, false);
 
-
-
-		//startActivity(new Intent(getActivity(),LineChartActivity.class));
-
-
-       // textView = (TextView) rootView.findViewById(R.id.words);
-        button= (Button) rootView.findViewById(R.id.click);
-//
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //textView.setText("you have clicked");
-				//startActivity(new Intent(getActivity(), SimpleScannerActivity.class));
-               // startActivity(new Intent(getActivity(), ScannerActivity.class));
-                Toast.makeText(getActivity(), "In progress..", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        dayoftheweek= (TextView) rootView.findViewById(R.id.weekDay);
+        breakfastFood = (TextView) rootView.findViewById(R.id.breakfastFood);
+        lunchFood = (TextView) rootView.findViewById(R.id.lunchFood);
+        dinnerFood = (TextView) rootView.findViewById(R.id.dinnerFood);
+        next = (ImageButton) rootView.findViewById(R.id.next);
+        previous = (ImageButton) rootView.findViewById(R.id.previous);
+        next.setOnClickListener(this);
+        previous.setOnClickListener(this);
 
 		return rootView;
 	}
+
+
+    @Override
+    public void onClick(View v) {
+        try {
+            switch (v.getId()) {
+                case R.id.next:
+                    counter++;
+
+                    if (counter < n && counter >= 0) {
+                        updatetext();
+                    } else {
+                        counter = 0;
+                        updatetext();
+                    }
+
+                    break;
+                case R.id.previous:
+                    counter--;
+
+
+                    if (counter <= n && counter >= 0) {
+                        updatetext();
+                    } else counter = n;
+
+
+//                        Toast.makeText(getActivity(), "In progress..", Toast.LENGTH_SHORT).show();
+//                        counter--;
+//                        dayoftheweek.setText(days[counter]);
+                    break;
+
+
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            Log.d("Meal plan", "ARRAYBUG" + e.toString());
+        }
+    }
+
+    private void updatetext () {
+           // Toast.makeText(getActivity(), "In progress..", Toast.LENGTH_SHORT).show();
+            dayoftheweek.setText(days[counter]);
+    }
+
 
 
 }
