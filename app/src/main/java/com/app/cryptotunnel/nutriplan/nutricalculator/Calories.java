7993abbc19f1f00d38f942a399de939551a4f055
codephillip <codephillip@gmail.com>
2015-08-02 14:01:13 +0300

    added the TextInputLayout and weightEditText in calories.java, added the correct teal accent colour

--- app/src/main/java/com/app/cryptotunnel/nutriplan/nutricalculator/Calories.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/nutricalculator/Calories.java
@@ -1,7 +1,9 @@
 package com.app.cryptotunnel.nutriplan.nutricalculator;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,71 +16,80 @@ import android.widget.TextView;
 import com.app.cryptotunnel.nutriplan.R;
 
 
-public class Calories extends Fragment implements RadioGroup.OnCheckedChangeListener {
+public class Calories extends Fragment {
+// implements RadioGroup.OnCheckedChangeListener {
 
     //    NumberPicker np;
 //    TextView tv1, tv2;
-    EditText weight, height, age;
-    Button calculate;
-    RadioGroup gender;
-    double w, h, a, calories;
-    TextView show;
+//    EditText weight, height, age;
+//    Button calculate;
+//    RadioGroup gender;
+//    double w, h, a, calories;
+//    TextView show;
+      EditText weight;
 
 
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.calories, container, false);
 
+        weight = (EditText) rootView.findViewById(R.id.weight);
 
-        weight = (EditText) rootView.findViewById(R.id.editText);
-        height = (EditText) rootView.findViewById(R.id.editText2);
-        age = (EditText) rootView.findViewById(R.id.editText3);
-        calculate = (Button) rootView.findViewById(R.id.calculate);
-        show = (TextView) rootView.findViewById(R.id.show);
-        gender = (RadioGroup) rootView.findViewById(R.id.gender);
+        TextInputLayout textInputLayout = (TextInputLayout) rootView.findViewById(R.id.text_input_layout).findViewById(R.id.weight);
+        textInputLayout.setErrorEnabled(true);
+        Resources res = getResources();
+        textInputLayout.setError(res.getString(R.string.weight_required));
 
-        calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                w = Double.parseDouble(String.valueOf(weight.getText()));
-//                h = Double.parseDouble(String.valueOf(height.getText()));
-//                a = Double.parseDouble(String.valueOf(age.getText()));
-//                caloriesm = ((10 * w) + (6.25 * h) - (5 * a) + 5);
-//                caloriesf = ((10 * w) + (6.25 * h) - (5 * a) - 161);
 
-                show.setText(String.valueOf(calories));
-            }
-        });
-
-        gender.setOnCheckedChangeListener(this);
+//        weight = (EditText) rootView.findViewById(R.id.editText);
+//        height = (EditText) rootView.findViewById(R.id.editText2);
+//        age = (EditText) rootView.findViewById(R.id.editText3);
+//        calculate = (Button) rootView.findViewById(R.id.calculate);
+//        show = (TextView) rootView.findViewById(R.id.show);
+//        gender = (RadioGroup) rootView.findViewById(R.id.gender);
+//
+//        calculate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                w = Double.parseDouble(String.valueOf(weight.getText()));
+////                h = Double.parseDouble(String.valueOf(height.getText()));
+////                a = Double.parseDouble(String.valueOf(age.getText()));
+////                caloriesm = ((10 * w) + (6.25 * h) - (5 * a) + 5);
+////                caloriesf = ((10 * w) + (6.25 * h) - (5 * a) - 161);
+//
+//                show.setText(String.valueOf(calories));
+//            }
+//        });
+//
+//        gender.setOnCheckedChangeListener(this);
 
 
         return rootView;
     }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch (checkedId) {
-            case R.id.male:
-                w = Double.parseDouble(String.valueOf(weight.getText()));
-                h = Double.parseDouble(String.valueOf(height.getText()));
-                a = Double.parseDouble(String.valueOf(age.getText()));
-                calories = ((10 * w) + (6.25 * h) - (5 * a) + 5);
-                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.4);
-                //show.setText(proteinResult.toString());
-                break;
-            case R.id.female:
-                w = Double.parseDouble(String.valueOf(weight.getText()));
-                h = Double.parseDouble(String.valueOf(height.getText()));
-                a = Double.parseDouble(String.valueOf(age.getText()));
-                calories = ((10 * w) + (6.25 * h) - (5 * a) - 161);
-                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.4);
-                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.6);
-                //show.setText(proteinResult.toString());
-                break;
-
-        }
-    }
+//
+//    @Override
+//    public void onCheckedChanged(RadioGroup group, int checkedId) {
+//        switch (checkedId) {
+//            case R.id.male:
+//                w = Double.parseDouble(String.valueOf(weight.getText()));
+//                h = Double.parseDouble(String.valueOf(height.getText()));
+//                a = Double.parseDouble(String.valueOf(age.getText()));
+//                calories = ((10 * w) + (6.25 * h) - (5 * a) + 5);
+//                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.4);
+//                //show.setText(proteinResult.toString());
+//                break;
+//            case R.id.female:
+//                w = Double.parseDouble(String.valueOf(weight.getText()));
+//                h = Double.parseDouble(String.valueOf(height.getText()));
+//                a = Double.parseDouble(String.valueOf(age.getText()));
+//                calories = ((10 * w) + (6.25 * h) - (5 * a) - 161);
+//                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.4);
+//                //proteinResult=(Double.parseDouble(weightEdit.getText().toString())*0.6);
+//                //show.setText(proteinResult.toString());
+//                break;
+//
+//        }
+//    }
 }
 //    public void show()
 //    {
