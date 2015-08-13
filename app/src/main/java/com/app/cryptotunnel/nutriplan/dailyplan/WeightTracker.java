34e13ff3a374a5d068fb94865d4d2e2b08c07650
codephillip <codephillip@gmail.com>
2015-08-13 03:40:05 +0300

    added a fab with child fabs in weight tracker

--- app/src/main/java/com/app/cryptotunnel/nutriplan/dailyplan/WeightTracker.java
+++ app/src/main/java/com/app/cryptotunnel/nutriplan/dailyplan/WeightTracker.java
@@ -1,6 +1,11 @@
 package com.app.cryptotunnel.nutriplan.dailyplan;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -9,19 +14,36 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
+import com.app.cryptotunnel.nutriplan.R;
 import com.app.cryptotunnel.nutriplan.customexception.InvalidValueException;
 import com.app.cryptotunnel.nutriplan.database.DatabaseHandler;
-import com.app.cryptotunnel.nutriplan.nutridiary.NoteEditorActivity;
-import com.app.cryptotunnel.nutriplan.R;
 import com.app.cryptotunnel.nutriplan.database.WeightTrackerContract;
+import com.app.cryptotunnel.nutriplan.nutridiary.NoteEditorActivity;
 
 public class WeightTracker extends Fragment implements  View.OnClickListener {
 	Button graph,history,save;
 	EditText currentWeight;
 	View rootView;
+
+	private static final String TAG = "Floating Action Button";
+	private static final String TRANSLATION_Y = "translationY";
+
+	private ImageButton fab;
+
+	private boolean expanded = false;
+
+	private View fabAction1;
+	private View fabAction2;
+	private View fabAction3;
+
+	private float offset1;
+	private float offset2;
+	private float offset3;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
@@ -32,9 +54,43 @@ public class WeightTracker extends Fragment implements  View.OnClickListener {
 		currentWeight = (EditText) rootView.findViewById(R.id.currentWeightEdit);
 		history = (Button) rootView.findViewById(R.id.history);
 		graph= (Button) rootView.findViewById(R.id.graph);
+		fabAction1 = rootView.findViewById(R.id.fab_action_1);
+		fabAction2 = rootView.findViewById(R.id.fab_action_2);
+		fabAction3 = rootView.findViewById(R.id.fab_action_3);
 		graph.setOnClickListener(this);
 		history.setOnClickListener(this);
 		save.setOnClickListener(this);
+		fabAction1.setOnClickListener(this);
+		fabAction2.setOnClickListener(this);
+		fabAction3.setOnClickListener(this);
+
+		final ViewGroup fabContainer = (ViewGroup) rootView.findViewById(R.id.fab_container);
+		fab = (ImageButton) rootView.findViewById(R.id.fab);
+
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				expanded = !expanded;
+				if (expanded) {
+					expandFab();
+				} else {
+					collapseFab();
+				}
+			}
+		});
+		fabContainer.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+			@Override
+			public boolean onPreDraw() {
+				fabContainer.getViewTreeObserver().removeOnPreDrawListener(this);
+				offset1 = fab.getY() - fabAction1.getY();
+				fabAction1.setTranslationY(offset1);
+				offset2 = fab.getY() - fabAction2.getY();
+				fabAction2.setTranslationY(offset2);
+				offset3 = fab.getY() - fabAction3.getY();
+				fabAction3.setTranslationY(offset3);
+				return true;
+			}
+		});
 
 		return rootView;
 	}
@@ -62,11 +118,25 @@ public class WeightTracker extends Fragment implements  View.OnClickListener {
 						e.printStringError(storedWeight);
 						Log.e("SQL BUG", e.toString());
 						//Snackbar.make(rootView, "Please enter your current weight", Snackbar.LENGTH_SHORT).show();
-						snackBar("Check Internet Connection");
+						snackBar("Please enter your current weight");
 					}
 				}else {
 					db.addWeight(new WeightTrackerContract(storedWeight, NoteEditorActivity.getTime()));
 				}
+				break;
+
+			case R.id.fab_action_1:
+				Log.d(TAG, "Action 1");
+				snackBar("Action 1");
+				break;
+			case R.id.fab_action_2:
+				Log.d(TAG, "Action 2");
+				snackBar("Action 2");
+
+				break;
+			case R.id.fab_action_3:
+				Log.d(TAG, "Action 3");
+				snackBar("Action 3");
 				break;
 		}
 	}
@@ -77,5 +147,42 @@ public class WeightTracker extends Fragment implements  View.OnClickListener {
 		View snackbarView = snackbar.getView();
 		snackbarView.setBackgroundColor(getResources().getColor(R.color.red));
 		snackbar.show();
+	}
+
+	private void collapseFab() {
+		fab.setImageResource(R.drawable.animated_minus);
+		AnimatorSet animatorSet = new AnimatorSet();
+		animatorSet.playTogether(createCollapseAnimator(fabAction1, offset1),
+				createCollapseAnimator(fabAction2, offset2),
+				createCollapseAnimator(fabAction3, offset3));
+		animatorSet.start();
+		animateFab();
+	}
+
+	private void expandFab() {
+		fab.setImageResource(R.drawable.animated_plus);
+		AnimatorSet animatorSet = new AnimatorSet();
+		animatorSet.playTogether(createExpandAnimator(fabAction1, offset1),
+				createExpandAnimator(fabAction2, offset2),
+				createExpandAnimator(fabAction3, offset3));
+		animatorSet.start();
+		animateFab();
+	}
+
+	private Animator createCollapseAnimator(View view, float offset) {
+		return ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, offset)
+				.setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+	}
+
+	private Animator createExpandAnimator(View view, float offset) {
+		return ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 0)
+				.setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+	}
+
+	private void animateFab() {
+		Drawable drawable = fab.getDrawable();
+		if (drawable instanceof Animatable) {
+			((Animatable) drawable).start();
+		}
 	}
 }
